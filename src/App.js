d5b95d3528ab56360e9c2b1683dc9cb65564b5f9
surya <suryajoshi617@gmail.com>
2023-09-06 14:29:29 +0530

    padding

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const Body = styled.div`
 const Wrapper = styled.div`
   background: linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
   width: 100%;
-  padding: 5rem
+  padding: 5rem 0;
 `
 function App() {
   const [darkMode, setDarkMode] = useState(true);
